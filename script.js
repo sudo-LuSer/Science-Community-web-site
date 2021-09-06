b8c9cddbda0257805bc5393feae6a16b26fbc37b
Nasroallah <65375745+sudo-LuSer@users.noreply.github.com>
2021-09-06 12:39:25 +0100

    update : dark mode design

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function share(){	
-	var arr=["Projects.." , "Proofs.." , "Exercices..","Information"];
-	var index=Math.floor(Math.random()*arr.length);
-	$('#share').innerHTML=arr[index];
-}
-setInterval(3000,share);
+var check=true;
+
+$('#dark-mode').click(function(){
+
+	if(check){
+
+		$(this).css('filter','invert(1)');
+
+		$('html').css('filter','invert(1)');
+
+		$('img').css('filter','invert(1)');
+
+		$('body').css('filter','invert(1)');
+
+		check=false;
+
+	}
+
+	else{
+
+		$(this).css('filter','invert(0)');
+
+		$('html').css('filter','invert(0)');
+
+		$('img').css('filter','invert(0)');
+
+		$('body').css('filter','invert(0)');
+
+		check=true;
+
+	}
+
+});
