21e6ef338e9a6dfc90531fc9d6b1669fd518a351
Nasroallah <65375745+sudo-LuSer@users.noreply.github.com>
2021-09-18 14:28:28 +0100

    Add files via upload

---
+++ darkModeScript.js
@@ -0,0 +1,40 @@
+var check=false;
+
+$('.DarkBtn').click(function(){
+
+	if(check==false){
+
+		$('span').css('filter','drop-shadow(2px 4px 8px var(--territory-text))');
+	
+		$('body').css('background','var(--red)');
+		
+		$('nav').css('background','var(--light-red)');
+		
+		$('li').css('border-bottom','5px var(--secondary-background) solid')
+		
+		$('#gitlink').css('color','var(--secondary-background)');
+		
+		$('li').hover(function(){
+		
+			$(this).css('color','var(--secondary-background)');
+		
+		});
+		
+		$('span').hover(function(){
+		
+			$(this).css('color','var(--secondary-background)');
+		
+		});
+
+		check=true;
+
+	}
+	
+	else{
+
+		location.reload();
+
+	}
+
+});
+
